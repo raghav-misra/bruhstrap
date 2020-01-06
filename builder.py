a3e5e5d481ed2dd486451447988bb3aaa9684922
Raghav Misra <raghav.m2014@gmail.com>
2020-01-05 19:12:01 -0800

    Add CSS Var compilation to builder script

--- builder.py
+++ builder.py
@@ -1,33 +1,37 @@
-# BUILDER CONFIGURATION
-BUILDER_CONFIG = {
-    "IN_DIR": "src",
-    "OUT_DIR": "dist",
-    "OUT_NAME": "bruhstrap",
-    "BUILD_ORDER": [
-        "main", "defaults", "utils", "layout", "elements", "transitions"
-    ]
-}
-# END BUILDER CONFIGURATION
 import requests
+import json
 
+# Read Builder Config from file:
+json_text = ""
+with open("builder.json", "r") as build_file:
+    json_text = build_file.read()
+
+BUILDER_CONFIG = json.loads(json_text)
+
+# Concatenate all of the files.
 new_data = ""
-for fname in BUILDER_CONFIG["BUILD_ORDER"]:
-    with open(f"{BUILDER_CONFIG['IN_DIR']}/{fname}.css", 'r') as file:
+for fname in BUILDER_CONFIG["buildOrder"]:
+    with open(f"{BUILDER_CONFIG['inDir']}/{fname}.css", 'r') as file:
         new_data = f"{new_data}\n{file.read()}"
         print(f"Added {fname} to bundle...")
 
-with open(f"{BUILDER_CONFIG['OUT_DIR']}/{BUILDER_CONFIG['OUT_NAME']}.css", "w") as outfile:
+# Unvariablize CSS vars:
+for name, value in BUILDER_CONFIG["cssVars"].items():
+    new_data = new_data.replace(f"var(--{name})", value)
+
+# Write unminified bundle to file.
+with open(f"{BUILDER_CONFIG['outDir']}/{BUILDER_CONFIG['outFilename']}.css", "w") as outfile:
     outfile.write(new_data)
     print("Wrote bundle to file...")
 
+# Minify bundle/write to file.
 print("Minifying bundle...")
-
 
 url = 'https://cssminifier.com/raw'
 data = {'input': new_data }
 response = requests.post(url, data=data)
 
-with open(f"{BUILDER_CONFIG['OUT_DIR']}/{BUILDER_CONFIG['OUT_NAME']}.min.css", "w") as min_outfile:
+with open(f"{BUILDER_CONFIG['outDir']}/{BUILDER_CONFIG['outFilename']}.min.css", "w") as min_outfile:
     min_outfile.write(response.text)
     print("Wrote minified css to file...")
 
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 # Bruhstrap:
 A CSS-only, minimal Bootstrap-esque library focused on utility classes. 
+
+## What is this?
+I like Bootstrap a lot, but jQuery, Popper, and its own script seems like a bit too much JavaScript for my taste. To resolve my personal problem, I embarked on an expedition to create a JavaScript-free Bootstrap! I was really lazy with this project, so I wrote a super tiny Python script to compile/minify the CSS for me.
+
+**Note:** I am aware of the existence of Bulma, but unfortunately I didn't like it as much as many people do (personal choice, please use Bulma guys).
